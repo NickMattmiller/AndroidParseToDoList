39d64a3388af635ebe811b0764fefc9fcda81d15
NickMattmiller <njmatt03@louisville.edu>
2015-11-08 22:36:19 -0500

    HM7 first commit
    added list, create, and update activity classes and xml files.

---
+++ app/src/main/java/com/nickmattmiller/androidparsetodolist/App.java
@@ -0,0 +1,14 @@
+package com.nickmattmiller.androidparsetodolist;
+
+import android.app.Application;
+import com.parse.Parse;
+
+
+public class App extends Application {
+
+    @Override public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(this, ApplicationID, ClientKey); // Your Application ID and Client Key are defined elsewhere
+    }
+}
